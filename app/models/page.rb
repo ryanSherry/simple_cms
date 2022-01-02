fd079bebb0cf7fe5ebf5e20cb810809e57df6f87
Ryan Sherry <rsherrycomputers@gmail.com>
2022-01-02 11:50:20 -0700

    updated routes

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,6 @@
 class Page < ApplicationRecord
 
-  belongs_to :subject, { :optional => false }
+  belongs_to :subject, optional: false
   has_many :sections
   has_and_belongs_to_many :admin_users
 
