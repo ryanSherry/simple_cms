b5e7f77ac5efe2aa2a4fc34abc4373b1707dfc00
Ryan Sherry <rsherrycomputers@gmail.com>
2022-01-05 16:06:52 -0700

    added some sexy validations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -9,5 +9,6 @@ class Subject < ApplicationRecord
   scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"]) }
 
   validates_presence_of :name
+  validates_length_of :name, :maximum => 255
 
 end
