a04b4fcf1054e968c435fb28d4c8f6fdf5e280ce
Ryan Sherry <rsherrycomputers@gmail.com>
2022-01-06 11:52:59 -0700

    playing with logging

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,6 +5,7 @@ class SubjectsController < ApplicationController
   before_action :subject_count, :only => [:show, :new, :create, :edit, :update]
 
   def index
+    logger.debug("*** Testing the logger. ***")
     @subjects = Subject.sorted
   end
 
